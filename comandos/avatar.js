5f83b33401310001ed1e4ee58bbc2710ea1b712d
YuryRegis <yury_regis@hotmail.com>
2020-04-20 15:03:48 -0300

    Comandos !avatar e !id melhorados
    *ID retorna um membro correspondente por ID;
    *avatar bug de menção corrigido;
    *inclusão do uso por moderador

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -1,20 +1,39 @@
 
 exports.run = async (client, message, args) => {
-    let admin = client.users.get("322421000153333761")
-    if(message.member.roles.some(r => r.name === "Admin") || message.member.roles.some(r => r.name === "Staff")) {
-        try {
-            let membro = await client.users.get(args[0])
-            let avatar = await membro.displayAvatarURL
-            let sender = message.author
-            await sender.send(`Avatar de ${membro.username}:\n${avatar}`)
-            await admin.send(`${message.author.username} solicitou avatar de ${membro.username}`)
-            return message.delete()
-        } catch(err) {
-            console.log(err);
-            message.reply(`Algo de errado não está certo!\nVerifique o comando digitado.`)
-        }
-    } else { message.reply(` você não possui permissão para usar este comando.`) }
-}
+    await message.delete();
+
+    let admin    = await client.users.get("322421000153333761"),
+        salaLogs = await message.guild.channels.get('698758957845446657');
+    
+        if(message.member.roles.some(r => r.name === "Admin"||r.name === "Staff"|| r.name==='Moderador')) {
+            try {
+                let membro   = {};
+                
+                if (/@/.test(args[0])) {
+                    membro = await message.mentions.members.first();
+                    if (!membro) return message.reply(`${args[0]} não encontrado`);
+                    membro = membro.user;
+                } else {
+                    membro = await client.users.get(args[0]);
+                    if (!membro) message.reply(`${args[0]} não encontrado`);
+                }
+                let avatar = await membro.displayAvatarURL
+                let sender = message.author
+    
+                await sender.send(`Avatar de ${membro.username}:\n${avatar}`)
+                    .then()
+                    .catch(err => salaLogs.send(`Terminal **avatar**:\`\`\`Alvo:      ${sender.username}\n${err}\`\`\``));
+    
+                await admin.send(`${message.author.username} solicitou avatar de ${membro.username}`);
+                return;
+    
+            }catch(err) {
+                console.log(err);
+                salaLogs.send(`Terminal **avatar**:\`\`\`Alvo:      ${args[0]}\n${err}\`\`\``);
+            }
+        } else message.reply(` você não possui permissão para usar este comando.`);
+    }
+
 
 exports.help = {
     name: "avatar"
--- comandos/id.js
+++ comandos/id.js
@@ -7,20 +7,30 @@ exports.run = async (client, message, args) => {
     message.delete();
     
     // console.log(args[0].split("@"));
-    if(message.member.roles.some(r => r.name === "Admin") || message.member.roles.some(r => r.name === "Staff")) {
+    if(message.member.roles.some(r => r.name === "Admin" || r.name === "Staff" || r.name==="Moderador")) {
         let regexSala = /#/;
         let canal = await client.channels.get("612399243520639016");
 
         if (regexSala.test(args[0])) {
             let procurado = await message.mentions.channels.first();
             message.author.send(`ID da sala: ${procurado.id}`);
-            return canal.send(`ID da sala enviado por DM ${message.author}`);
+            return canal.send(`ID da sala ${procurado} enviado por DM ${message.author}`);
         }
         
-        let idusuario = args[0].split("@")[1].split(">")[0];
-        await message.author.send(`O ID do usuário que deseja é ${idusuario}`);
+        if (/@/.test(args[0])) {
+            let idusuario = args[0].split("@")[1].split(">")[0];
+            await message.author.send(`O ID do usuário que deseja é ${idusuario}`);
+            return canal.send(`ID enviado por DM ${message.author}`);
+        }
 
-        return canal.send(`ID enviado por DM ${message.author}`);
+        if(!isNaN(args[0])) {
+            let alvo = message.channel.guild.members.get(args[0]);
+            if (alvo === undefined) return message.reply(`${args[0]} não pertence a nenhum membro.`);
+            message.author.send(`ID de ${alvo.displayName}:   ${alvo.id}`);
+            let texto = `${message.author.username} localizei um membro com este ID:`
+            canal.send(texto+`\`\`\`${alvo.id}:   ${alvo.displayName}\`\`\``);
+        }
+
     } 
     else return message.reply(" você não tem permissão para usar este comando."); 
 }
