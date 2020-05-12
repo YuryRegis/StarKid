69ccfb40c649b19c9367db4bdd48ee5343c1e402
YuryRegis <yury_regis@hotmail.com>
2020-05-12 03:44:47 -0300

    Menciona autor do comando

--- comandos/listpasses.js
+++ comandos/listpasses.js
@@ -5,11 +5,12 @@ exports.run = async (client, message, args) => {
     await message.delete();
 
     const salaLogs = await message.guild.channels.get('698758957845446657'),
+          autor    = await message.author,
           consulta = await dbListPasses(),
           sorteio  = args[0];
     
     if(sorteio===undefined || isNaN(sorteio))
-        return salaLogs.send(`${message.author} ${sorteio} não é um concurso válido.`)
+        return salaLogs.send(`${message.author} ${sorteio} não é um concurso válido.`);
 
 
     let listaPasses = ``,
@@ -17,16 +18,16 @@ exports.run = async (client, message, args) => {
         listaCupons = consulta.filter(filtro);
 
 
-    listaCupons.forEach(element => listaPasses += `${element.cupom} `)
+    listaCupons.forEach(element => listaPasses += `${element.cupom} `);
 
     if (listaPasses.length === 0)
-        salaLogs.send(`Nenhum cupom encontrado para o concurso ${sorteio}.`);
+        salaLogs.send(`${autor}: Nenhum cupom encontrado para o concurso ${sorteio}.`);
     else
-        salaLogs.send(`Cupons do concurso ${sorteio}\n\`\`\`diff\n${listaPasses}\n\`\`\``);
+        salaLogs.send(`${autor}: Cupons do concurso ${sorteio}\n\`\`\`diff\n${listaPasses}\n\`\`\``);
 
 }
 
 
 exports.help = {
-    name: "rmvvip"
+    name: "listpasses"
 }
