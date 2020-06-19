93ff84a5ac43d304bfa76b7002fd089344dc901b
Harry <yury_regis@hotmail.com>
2020-06-19 16:06:30 -0300

    v1.1
    Permissão para uso do comando apenas na sala #Flood_de_comandos

--- comandos/dropar.js
+++ comandos/dropar.js
@@ -2,12 +2,20 @@ const { dbListDrops, dbDeleteDrop } = require('../Routes/rotaDrop');
 
 
 exports.run = async (client, message, args) => {
-    const chave    = args[0],
-          autor    = message.author,
-          drops    = await dbListDrops(),
-          salaBot  = await message.guild.channels.get('634200679224967188'),
-          salaLogs = await message.guild.channels.get('698758957845446657');
-
+    const chave      = args[0],
+          autor      = message.author,
+          drops      = await dbListDrops(),
+          salaFlood  = await message.guild.channels.get('653744153171066880'),
+          salaBot    = await message.guild.channels.get('634200679224967188'),
+          salaLogs   = await message.guild.channels.get('698758957845446657'),
+          perm       = await message.member.roles.some(r => r.name === "Admin") || 
+                             message.member.roles.some(r => r.name === "Staff");
+    
+    if(!perm && message.channel.id !== salaFlood.id) {
+        message.reply(`Use este comando apenas no canal ${salaFlood}`);
+        return message.delete();
+    }
+    
     if(chave === undefined)
         return message.reply('Para dropar um cupom de sorteio, você precisa informar uma chave válida.');
     
